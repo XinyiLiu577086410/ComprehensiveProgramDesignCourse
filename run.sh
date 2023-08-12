0ec96335db28c90196641e0bd2e3af45cb46d781
XinyiLiu577086410 <577086410@.com>
2023-08-12 18:22:28 +0800

    反向ls（ls ./testset -r）

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ mkdir output
 g++ ./src/main.cpp -o ./executable/dpll -std=c++11 -O0 
 # g++ ./src/Verify.cpp -o ./executable/verify -std=c++11
 echo Snapshot:
-for file in `ls ./testset` 
+for file in `ls ./testset -r` 
     do
     echo "\nSolving ${file}"
     ./executable/dpll "${file}"
